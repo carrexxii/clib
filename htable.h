77328ad2be6c4c9db6fe4f2ed39100398ed5fb6f
carrexxii <cjbarrett101@outlook.com>
2024-04-08 18:25:05 +0200

    Change HTable to use String

--- htable.h
+++ htable.h
@@ -9,25 +9,25 @@
  */
 
 struct HPair {
-	char* key;
-	int64 val;
+	String key;
+	int64  val;
 	struct HPair* next;
 };
 
 struct HTable {
-	struct Arena arena;
+	struct Arena arena; // FIXME - can't do this
 	bool arena_is_external;
 	isize cap;
 	struct HPair* pairs;
 };
 
 struct HTable htable_new(isize count, struct Arena* arena);
-uint64        htable_hash(struct HTable* restrict htable, char* restrict str);
-struct HPair* htable_insert(struct HTable* restrict htable, char* restrict key, int64 val);
-struct HPair* htable_get_pair(struct HTable* htable, char* key);
-int64         htable_get(struct HTable* restrict htable, char* restrict key);
-int64         htable_get_or_insert(struct HTable* restrict htable, char* restrict key, int64 val);
-int           htable_set(struct HTable* restrict htable, char* restrict key, int64 val);
+uint64        htable_hash(struct HTable* htable, String str);
+struct HPair* htable_insert(struct HTable* htable, String key, int64 val);
+struct HPair* htable_get_pair(struct HTable* htable, String key);
+int64         htable_get(struct HTable* htable, String key);
+int64         htable_get_or_insert(struct HTable* htable, String key, int64 val);
+int           htable_set(struct HTable* htable, String key, int64 val);
 void          htable_print(struct HTable* htable);
 void          htable_free(struct HTable* htable);
 
@@ -50,26 +50,24 @@ struct HTable htable_new(isize count, struct Arena* arena)
 }
 
 /* PJW Hash Function: https://www.partow.net/programming/hashfunctions/ */
-uint64 htable_hash(struct HTable* restrict htable, char* restrict str)
+uint64 htable_hash(struct HTable* htable, String str)
 {
 	uint64 hash = 1315423911;
-	char* c = str;
+	char* c = str.data;
 	while (*c)
 		hash ^= ((hash << 5) + (*c++) + (hash >> 2));
 
 	return hash % htable->cap;
 }
 
-struct HPair* htable_insert(struct HTable* restrict htable, char* restrict key, int64 val)
+struct HPair* htable_insert(struct HTable* htable, String key, int64 val)
 {
-	assert(key);
-
 	struct HPair* pair = &htable->pairs[htable_hash(htable, key)];
 	/* Only search through the list if first slot is not available */
-	if (pair->key) {
+	if (pair->key.data) {
 		do {
 			/* Check for duplicate keys */
-			if (!pair->key || !strcmp(pair->key, key))
+			if (!pair->key.data || !strcmp(pair->key.data, key.data))
 				break;
 			if (!pair->next) {
 				pair->next = arena_alloc(&htable->arena, sizeof(struct HPair));
@@ -80,25 +78,21 @@ struct HPair* htable_insert(struct HTable* restrict htable, char* restrict key, 
 			pair = pair->next;
 		} while (pair);
 	}
-	if (!pair->key) {
-		isize len = strlen(key);
-		pair->key = arena_alloc(&htable->arena, (len + 1)*sizeof(char));
-		strcpy(pair->key, key);
-		pair->key[len] = '\0';
-	}
+	if (!pair->key.data)
+		pair->key = string_copy(key, &htable->arena);
 	pair->val = val;
 
 	return pair;
 }
 
 /* Returns NULL if the key was not found */
-struct HPair* htable_get_pair(struct HTable* restrict htable, char* restrict key)
+struct HPair* htable_get_pair(struct HTable* htable, String key)
 {
 	struct HPair* pair = &htable->pairs[htable_hash(htable, key)];
-	if (!pair->key)
+	if (!pair->key.data)
 		return NULL;
 
-	while (strcmp(pair->key, key)) {
+	while (strcmp(pair->key.data, key.data)) {
 		pair = pair->next;
 		if (!pair)
 			return NULL;
@@ -108,7 +102,7 @@ struct HPair* htable_get_pair(struct HTable* restrict htable, char* restrict key
 }
 
 /* Returns -1 if the key was not found */
-int64 htable_get(struct HTable* restrict htable, char* restrict key)
+int64 htable_get(struct HTable* htable, String key)
 {
 	struct HPair* pair = htable_get_pair(htable, key);
 	return pair? pair->val
@@ -116,7 +110,7 @@ int64 htable_get(struct HTable* restrict htable, char* restrict key)
 }
 
 /* Same as htable_get, but will insert the value if the key is not present */
-int64 htable_get_or_insert(struct HTable* restrict htable, char* restrict key, int64 val)
+int64 htable_get_or_insert(struct HTable* htable, String key, int64 val)
 {
 	struct HPair* pair = htable_get_pair(htable, key);
 	return pair? pair->val
@@ -124,7 +118,7 @@ int64 htable_get_or_insert(struct HTable* restrict htable, char* restrict key, i
 }
 
 /* Returns 0 if the value was set or -1 if the key was not found */
-int htable_set(struct HTable* restrict htable, char* restrict key, int64 val)
+int htable_set(struct HTable* htable, String key, int64 val)
 {
 	// TODO: This should insert if its not there (ie, combine with htable_insert())
 	struct HPair* pair = htable_get_pair(htable, key);
@@ -142,10 +136,10 @@ void htable_print(struct HTable* htable)
 	struct HPair* pair;
 	for (int i = 0; i < htable->cap; i++) {
 		pair = &htable->pairs[i];
-		fprintf(DEBUG_OUTPUT, "\t[%s: %ld]", pair->key? pair->key: "", pair->val);
+		fprintf(DEBUG_OUTPUT, "\t[%s: %ld]", pair->key.data? pair->key.data: "", pair->val);
 		while (pair->next) {
 			pair = pair->next;
-			fprintf(DEBUG_OUTPUT, " -> [%s: %ld]", pair->key, pair->val);
+			fprintf(DEBUG_OUTPUT, " -> [%s: %ld]", pair->key.data, pair->val);
 		}
 		fprintf(DEBUG_OUTPUT, "\n");
 	}
@@ -153,7 +147,7 @@ void htable_print(struct HTable* htable)
 
 void htable_free(struct HTable* htable)
 {
-	if (htable->arena_is_external)
+	if (htable->arena_is_external) // FIXME
 		arena_free(&htable->arena);
 	sfree(htable->pairs);
 }
